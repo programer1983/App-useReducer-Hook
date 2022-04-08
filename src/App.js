67e4c6ab7797bff7218f8a72b37eeb4c048f83ee
programer1983 <programer972@gmail.com>
2022-04-08 13:20:55 +0300

    commit-3;

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-
+import {useState, useReducer} from "react"
 import './App.css';
 
 function App() {
+  const [counter, setCounter] = useState(0)
+
+  const incremaent = () => {
+    setCounter(counter + 1) 
+  }
+
+  const decremaent = () => {
+    setCounter(counter - 1) 
+  }
+
+
   return (
-    <div className="App">
-     hello
+    <div className="App border">
+      <h1>useReducer Hook</h1>
+      <h2>{counter}</h2>
+      <button onClick={incremaent}>Incremaent</button>
+      <button onClick={decremaent}>Decremaent</button>
     </div>
   );
 }
